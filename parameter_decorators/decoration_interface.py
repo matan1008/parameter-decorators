554759089251703491dd91c173c8bcf91d2b72cc
matan <matan1008@gmail.com>
2023-02-07 08:21:44 +0200

    DecorationInterface: Exclude from codecov

--- parameter_decorators/decoration_interface.py
+++ parameter_decorators/decoration_interface.py
@@ -6,16 +6,16 @@ from typing import Any
 class DecorationInterface(ABC):
     @abstractmethod
     def validate(self, param: Any, signature_item: Parameter) -> bool:
-        return False
+        return False  # pragma: no cover
 
     @abstractmethod
     def convert(self, param: Any, signature_item: Parameter) -> Any:
-        return None
+        return None  # pragma: no cover
 
     @abstractmethod
     def reannotation_validate(self, signature_item: Parameter) -> bool:
-        return False
+        return False  # pragma: no cover
 
     @abstractmethod
     def reannotate(self, signature_item: Parameter) -> Any:
-        return None
+        return None  # pragma: no cover
